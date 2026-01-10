f73eb13486ba78eb9df5e7622ecb34547425387e
MohdAyaanSiddiqui <muhammadxayaan007@gmail.com>
2026-01-10 14:40:00 +0530

    Initial Updates

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -1,13 +1,6 @@
 import OpenAI from "openai"
 import sql from "../configs/db.js";
-import axios from "axios";
 import { v2 as cloudinary } from "cloudinary";
-import fs from "fs";
-import FormData from "form-data";
-import { createRequire } from "module";
-
-const require = createRequire(import.meta.url);
-const pdfParse = require("pdf-parse");
 
 const AI = new OpenAI({
     apiKey: process.env.GEMINI_API_KEY,
@@ -84,47 +77,6 @@ export const generateBlogTitle = async (req, res) => {
     }
 }
 
-
-export const generateImage = async (req, res) => {
-    try {
-        const { userId } = await req.auth();
-        const { prompt, publish } = req.body;
-        
-        if(!prompt){
-            return res.status(400).json({success: false, message: "Prompt Required"})
-        }
-
-        const formData = new FormData()
-        formData.append('prompt', prompt);
-        
-        const response = await axios.post("https://clipdrop-api.co/text-to-image/v1", formData, {
-            headers: {
-                "x-api-key": process.env.CLIPDROP_API_KEY, 
-            },
-            responseType: "arraybuffer",
-        }
-    )
-
-        const base64Image = `data:image/png;base64,${Buffer.from(response.data).toString('base64')}`;
-
-        const { secure_url } = await cloudinary.uploader.upload(base64Image,{
-        resource_type: "image"
-        })
-
-        await sql`INSERT INTO creations (user_id, prompt, content, type, publish) VALUES (${userId}, ${prompt}, ${secure_url}, 'image', ${publish ?? false})`;
-
-        res.json({
-             success: true, 
-             content: secure_url, 
-             message:"Image Generated Successfully" 
-        });
-        
-    } catch (error) {
-        console.error("Image Error" , error.response?.data ||error.message)
-        res.status(500).json({ success: false, message: "Image Generation Failed"})
-    }
-}
-
 export const removeImageBackground = async (req, res) => {
     try {
         const { userId } = await req.auth();
@@ -180,41 +132,3 @@ export const removeImageObject = async (req, res) => {
     }
 }
 
-export const resumeReview = async (req, res) => {
-    try {
-        const { userId } = await req.auth();
-        const resume = req.file;
-
-        if (resume.size > 5 * 1024 * 1024) {
-            return res.json({ success: false, message: "Resume File Size Exceeds allowed size (5MB)." })
-        }
-
-        const dataBuffer = fs.readFileSync(resume.path);
-        const pdfData = await pdfParse(dataBuffer);
-
-        const prompt = `Review the following resume and provide constructive feedback on its strengths, weakness, and areas for improvement. Resume Content:\n\n${pdfData.text}`
-        
-        const response = await AI.chat.completions.create({
-            model: "gemini-2.0-flash",
-            messages: [{ 
-                role: "user",
-                content: prompt, 
-            },],
-            temperature: 0.7,
-            max_tokens: 1000
-        });
-        const content = response.choices[0].message.content
-        
-        await sql`INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId},'Review The Uploaded Resume', ${content}, 'resume-review')
-        `;
-
-        res.json({ 
-            success: true, 
-            content,
-            message:"Review Resume Successfully" })
-
-    } catch (error) {
-        console.log(error.message)
-        res.json({ success: false, message: error.message })
-    }
-}
